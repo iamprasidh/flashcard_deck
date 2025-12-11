c9916b325941decf7da26b82d0231fe4637e3ac4
iamprasidh <iamprasidh@gmail.com>
2025-12-11 19:28:17 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flashcard_deck/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flashcard_deck
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/flashcard.dart
@@ -0,0 +1,37 @@
+class Flashcard {
+  final String id;
+  final String question;
+  final String answer;
+  final bool isMastered;
+
+  Flashcard({
+    required this.id,
+    required this.question,
+    required this.answer,
+    required this.isMastered,
+  });
+
+
+  Flashcard.fromJson(Map<String, dynamic> json) {
+    return Flashcard(
+      id: json['id'] as String,
+      question: json['question'] as String,
+      answer: json['answer'] as String,
+      isMastered: json['isMastered'] as bool? ?? false,
+    );
+  }
+
+  Flashcard copywith({
+    String? id,
+    String? question,
+    String? answer,
+    bool? isMastered,
+  }) {
+    return Flashcard(
+      id: id ?? this.id,
+      question: question ?? this.question,
+      answer: answer ?? this.answer,
+      isMastered: isMastered ?? this.isMastered,
+    );
+  }
+}
