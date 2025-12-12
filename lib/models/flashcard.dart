a2eb7c91305309251a160bb12a9460072678809f
iamprasidh <iamprasidh@gmail.com>
2025-12-12 16:48:21 +0530

    Prepared Flashcard Model for Hive

--- lib/models/flashcard.dart
+++ lib/models/flashcard.dart
@@ -1,7 +1,18 @@
+import 'package:hive/hive.dart'; // Import Hive
+
+part 'flashcard.g.dart'; // REQUIRED: tells Dart to look for the generated file
+
+// Add the HiveType annotation with a unique ID for this model (e.g., 0)
+@HiveType(typeId: 0)
 class Flashcard {
+  // Use the HiveField annotation for each property you want to save
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String question;
+  @HiveField(2)
   final String answer;
+  @HiveField(3)
   final bool isMastered;
 
   Flashcard({
